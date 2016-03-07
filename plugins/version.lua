0ba5de96791be166980690724d3319b90f7bbcc4
mrrigealijan <ccaahh430@gmail.com>
2016-03-07 18:48:18 +0000

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return 'rabin Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\n#\n\nDevelopers :\n@ali_vilter\n\nSupport :\n https://telegram.me/joinchat/CZaDBTzv9se87cufoCkkkg' 
+  --[[ Checkout https://github.com/Josepdal/DBTeam
+  --GNU GPL v2 license.]]
+end
+
+return {
+  patterns = {
+    "^#version$"
+  }, 
+  run = run 
+}
+
+end
